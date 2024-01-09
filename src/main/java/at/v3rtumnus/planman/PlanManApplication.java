ecee711170b4ba070ecb519b7708b316322ebf8e
v3rtumnus <mistrasser1@gmx.at>
2024-01-09 20:59:03 +0100

    use selenium for yahoo finance quote retrieval

--- src/main/java/at/v3rtumnus/planman/PlanManApplication.java
+++ src/main/java/at/v3rtumnus/planman/PlanManApplication.java
@@ -1,6 +1,7 @@
 package at.v3rtumnus.planman;
 
 import at.v3rtumnus.planman.service.YahooFinanceService;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.lang3.SystemUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,12 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class PlanManApplication {
 
 	public static void main(String[] args) {
-		if (SystemUtils.OS_NAME.startsWith("Windows")) {
-			System.setProperty("webdriver.chrome.driver", PlanManApplication.class.getClassLoader().getResource("webdriver/chromedriver.exe").getPath());
-		} else {
-			System.setProperty("webdriver.chrome.driver", "/tmp/chromedriver");
-		}
-
+		WebDriverManager.chromedriver().setup();
 
 		SpringApplication.run(PlanManApplication.class, args);
 	}
