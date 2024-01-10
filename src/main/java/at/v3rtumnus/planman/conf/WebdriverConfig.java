5fdb98d50263f61524a9efffc428b05625e103c5
v3rtumnus <mistrasser1@gmx.at>
2024-01-10 21:10:57 +0100

    switch to remote driver

--- src/main/java/at/v3rtumnus/planman/conf/WebdriverConfig.java
+++ src/main/java/at/v3rtumnus/planman/conf/WebdriverConfig.java
@@ -1,15 +1,19 @@
 package at.v3rtumnus.planman.conf;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @Configuration
 public class WebdriverConfig {
 
     @Bean
-    public WebDriver webDriver() {
-        return WebDriverManager.chromedriver().browserInDocker().create();
+    public WebDriver webDriver() throws MalformedURLException {
+        return new RemoteWebDriver(new URL("http://localhost:4444"), new ChromeOptions());
     }
 }
